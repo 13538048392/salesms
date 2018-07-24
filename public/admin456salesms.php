c2bee2bf3513a0f5c1bbad0a69a248d21d6b13c7
13538048392 <1642504508@qq.com>
2018-07-24 16:46:27 +0800

    admin456salesms.php add 禁用后台路由

--- public/admin456salesms.php
+++ public/admin456salesms.php
@@ -15,3 +15,5 @@ define('APP_PATH', __DIR__ . '/../application/');
 define("BIND_MODULE",'admin');
 // 加载框架引导文件
 require __DIR__ . '/../thinkphp/start.php';
+//关闭admin后台模块的路由
+\think\App::route(false);

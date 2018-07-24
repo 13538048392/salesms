a803227ccc9f755fb92f17821ad0eeb17a8df42d
13538048392 <1642504508@qq.com>
2018-07-24 11:13:45 +0800

    mod 前后台入口文件

--- public/admin456salesms.php
+++ public/admin456salesms.php
@@ -12,6 +12,6 @@
 
 // 定义应用目录
 define('APP_PATH', __DIR__ . '/../application/');
+define("BIND_MODULE",'admin');
 // 加载框架引导文件
-define("BIND_MODULE",'index');
 require __DIR__ . '/../thinkphp/start.php';

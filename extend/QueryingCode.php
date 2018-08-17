6b6cbd651bd09e5332d8ec5f8cee952e777c3fd4
hannan <langxianwuxue@.163.com>
2018-08-17 09:59:08 +0800

    qrcode

--- extend/QueryingCode.php
+++ extend/QueryingCode.php
@@ -8,19 +8,19 @@
 require_once (ROOT_PATH  . 'extend/phpqrcode/phpqrcode.php');
 class QueryingCode
 {
-   public  function makeQueryingCode($url=''){
+   public  function makeQueryingCode($url='',$logoPath){
        $value = $url;					//二维码内容
        $errorCorrectionLevel = 'H';	//容错级别
        $matrixPointSize = 6;			//生成图片大小
        //生成二维码图片
-       $filename = 'qrcode/'.microtime().'.png';
-       QRcode::png($value,$filename , $errorCorrectionLevel, $matrixPointSize, 2);
-
-       $logo = 'qrcode/logo.jpg'; 	//准备好的logo图片
-       $QR = $filename;			//已经生成的原始二维码图
-
-       if (file_exists($logo)) {
-           $QR = imagecreatefromstring(file_get_contents($QR));   		//目标图象连接资源。
+       ob_start();
+       QRcode::png($value,false , $errorCorrectionLevel, $matrixPointSize, 2);
+       $ob_contents = ob_get_contents(); //读取缓存区数据
+       ob_end_clean();
+       $QR = imagecreatefromstring($ob_contents);
+       $logo = $logoPath;//准备好的logo图片
+       if ($logo!==false) {
+           $QR = imagecreatefromstring($ob_contents);   		//目标图象连接资源。
            $logo = imagecreatefromstring(file_get_contents($logo));   	//源图象连接资源。
            $QR_width = imagesx($QR);			//二维码图片宽度
            $QR_height = imagesy($QR);			//二维码图片高度
@@ -30,18 +30,17 @@ class QueryingCode
            $scale = $logo_width/$logo_qr_width;   	//logo的宽度缩放比(本身宽度/组合后的宽度)
            $logo_qr_height = $logo_height/$scale;  //组合之后logo的高度
            $from_width = ($QR_width - $logo_qr_width) / 2;   //组合之后logo左上角所在坐标点
-
            //重新组合图片并调整大小
            /*
             *	imagecopyresampled() 将一幅图像(源图象)中的一块正方形区域拷贝到另一个图像中
             */
            imagecopyresampled($QR, $logo, $from_width, $from_width, 0, 0, $logo_qr_width,$logo_qr_height, $logo_width, $logo_height);
-       }
 
+       }
        //输出图片
-       imagepng($QR, 'qrcode.png');
-       imagedestroy($QR);
-       imagedestroy($logo);
+       Header("Content-type: image/png");
+       imagepng($QR);
+       exit;
     }
     public function makeQrCode($url='')
     {

f0b50a21e08f987a339f940eb4724c2d4dbeeb19
祈 <yuqi2060@163.com>
2018-08-28 18:36:50 +0800

    api qr_code

--- extend/QueryingCode.php
+++ extend/QueryingCode.php
@@ -55,6 +55,38 @@ class QueryingCode
        
      }
 
+     public function createQrCode($url,$logo_url){
+      //生成二维码
+      $value = $url; //二维码内容 
+      $errorCorrectionLevel = 'L';//容错级别 
+      $matrixPointSize = 6.8;//生成图片大小 
+      $file = 'qr_code/'.microtime().'.png';
+      //生成二维码图片 
+       QRcode::png($value,$file, $errorCorrectionLevel, $matrixPointSize, 2);
+       $QR = $file;
+      $logo = $logo_url;//准备好的logo图片  
+      if ($logo !== FALSE) { 
+      $QR = imagecreatefromstring(file_get_contents($QR)); 
+      $logo = imagecreatefromstring(file_get_contents($logo)); 
+      $QR_width = imagesx($QR);//二维码图片宽度 
+      $QR_height = imagesy($QR);//二维码图片高度 
+      $logo_width = imagesx($logo);//logo图片宽度 
+      $logo_height = imagesy($logo);//logo图片高度 
+      $logo_qr_width = $QR_width / 5; 
+      $scale = $logo_width/$logo_qr_width; 
+      $logo_qr_height = $logo_height/$scale; 
+      $from_width = ($QR_width - $logo_qr_width) / 2; 
+      //重新组合图片并调整大小 
+      imagecopyresampled($QR, $logo, $from_width, $from_width, 0, 0, $logo_qr_width, 
+      $logo_qr_height, $logo_width, $logo_height); 
+
+      } 
+      //输出图片 
+      imagepng($QR, $file); 
+      return "http://$_SERVER[SERVER_NAME]/$file";
+      // echo "<img src='http://$_SERVER[SERVER_NAME]/$file'>"; 
+     }
+
     public function makeQrCode($url='')
     {
         QRcode::png($url);

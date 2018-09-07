2180e203ac95b72651e3552153276e7889770a76
hannan <langxianwuxue@.163.com>
2018-09-07 09:00:27 +0800

    tel

--- application/index/controller/Register.php
+++ application/index/controller/Register.php
@@ -229,7 +229,7 @@ class Register extends Base
      */
     private function isMobile($mobile)
     {
-        if (preg_match('/^((13[0-9])|(14[5|7])|(15([0-3]|[5-9]))|(18[0,5-9]))\d{8}$/', $mobile))
+        if (preg_match('/^((13[0-9])|(14[5|7])|(15([0-3]|[5-9]))|(18[0,5-9])|(17[0,9])|(19[0,9]))\d{8}$/', $mobile))
             return true;
         return false;
     }

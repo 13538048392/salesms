2e3213aae277809c929023c63fd44a11b64ad321
hannan <langxianwuxue@.163.com>
2018-10-23 15:02:13 +0800

    log

--- application/index/controller/Transaction.php
+++ application/index/controller/Transaction.php
@@ -145,12 +145,16 @@ class Transaction extends Base
     public function log($type, $content)
     {
         $filename = $_SERVER['DOCUMENT_ROOT'] . '/../application/errorlog';
-        if (!file_exists($filename)) {
-            mkdir($filename);
-            chmod($filename, 777);
+        if (!is_dir($filename)) {
+            mkdir($filename, 0777);
         }
         $filename = $filename . '/' . $type . '.txt';
-        $Ts = fopen($filename, "a+");
+        if (!file_exists($filename)) {
+            if (!is_writetable($filename)) {
+                chmod($fileName, 0777);
+            }
+            $Ts = fopen($filename, "a+");
+        }
         fputs($Ts, "执行日期：" . "\r\n" . date('Y-m-d H:i:s', time()) . ' ' . "\n" . $content . "\n");
         fclose($Ts);
     }
